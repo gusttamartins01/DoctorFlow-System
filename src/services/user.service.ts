7822151d107512637381471df46ddbfe115ed02f
gustavo martins <luizgustams@gmail.com>
2026-07-06 20:32:57 -0300

    feat: Criar função para selecionar os  usuarios por ID

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,3 +8,19 @@ export async function findUsers(): Promise<User[]> {
 
 	return users as User[];
 }
+
+export async function findUserById(id: number): Promise<User | undefined> {
+	const db = await connectDatabase();
+
+	const smt = await db.prepare(
+		`
+        SELECT * FROM user WHERE id = ?
+        `,
+	);
+
+	const user = await smt.get(id);
+
+	await smt.finalize();
+
+	return user as User | undefined;
+}
