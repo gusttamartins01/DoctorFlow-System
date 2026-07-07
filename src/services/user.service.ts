23f8493c0a00f622b4643b0f085299a328eea7a1
gustavo martins <luizgustams@gmail.com>
2026-07-07 18:30:21 -0300

    feat: criar função para criar usuario

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { connectDatabase } from '../database/connection.ts';
-import type { User } from '../types.ts';
+import type { CreateUser, User } from '../types.ts';
 
 export async function findUsers(): Promise<User[]> {
 	const db = await connectDatabase();
@@ -23,4 +23,36 @@ export async function findUserById(id: number): Promise<User | undefined> {
 	await smt.finalize();
 
 	return user as User | undefined;
+}
+
+export async function insertUser({
+	name,
+	cpf,
+	email,
+	phone,
+}: CreateUser): Promise<User> {
+	if (!name || !cpf || !email || !phone)
+		throw new Error('Todos os campos precisam ser preenchidos.');
+
+	const db = await connectDatabase();
+
+	const result = await db.run(
+		`
+        INSERT INTO user (name, cpf, email, phone)
+        VALUES (?, ?, ?, ?)
+        `,
+		[name, cpf, email, phone],
+	);
+
+	if (!result || result.lastID === undefined) {
+		throw new Error('Falha ao criar usuário.');
+	}
+
+	return {
+		id: result.lastID,
+		name,
+		cpf,
+		email,
+		phone,
+	} as User;
 }
