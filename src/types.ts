889f3a5465a741a6d9a2ef241158fdb3c586e739
gustavo martins <luizgustams@gmail.com>
2026-07-07 18:31:13 -0300

    feat: criar tipo reutilizavel de criar usuario

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type User = {
+	id: number;
+	name: string;
+	cpf: string;
+	email: string;
+	phone: string;
+	is_active: boolean;
+	created_at: string;
+	update_at: string;
+};
+
+export type CreateUser = Omit<
+	User,
+	'id' | 'is_active' | 'created_at' | 'update_at'
+>;
