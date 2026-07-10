e0e0fa1deda8404388b259aec616249ebdf2538d
gustavo martins <luizgustams@gmail.com>
2026-07-10 19:27:54 -0300

    feat: criar controller de criar atualizar um usuário

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
 import * as UserService from '../services/user.service.ts';
-import type { CreateUser } from '../types.ts';
+import type { CreateUser, UpdateUser } from '../types.ts';
 
 export async function getAllUser(_request: Request, response: Response) {
 	const users = await UserService.findUsers();
@@ -27,4 +27,19 @@ export async function createUser(request: Request, response: Response) {
 	});
 
 	response.status(201).json(user);
+}
+
+export async function updateUser(request: Request, response: Response) {
+	const id = Number(request.params.id);
+	const { name, cpf, email, phone } = request.body as UpdateUser;
+
+	const user = await UserService.modifyUser({
+		id,
+		name,
+		cpf,
+		email,
+		phone,
+	});
+
+	response.status(200).json(user);
 }
