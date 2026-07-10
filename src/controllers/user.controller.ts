b4fb62062d7aafd4c49c412da2af48c4dee8da35
gustavo martins <luizgustams@gmail.com>
2026-07-10 19:27:23 -0300

    feat: criar controller de criar um novo usuário

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import * as UserService from '../services/user.service.ts';
+import type { CreateUser } from '../types.ts';
 
 export async function getAllUser(_request: Request, response: Response) {
 	const users = await UserService.findUsers();
@@ -13,4 +14,17 @@ export async function getUserById(request: Request, response: Response) {
 	const user = await UserService.findUserById(id);
 
 	response.status(200).json(user);
+}
+
+export async function createUser(request: Request, response: Response) {
+	const { name, cpf, email, phone } = request.body as CreateUser;
+
+	const user = await UserService.insertUser({
+		name,
+		cpf,
+		email,
+		phone,
+	});
+
+	response.status(201).json(user);
 }
